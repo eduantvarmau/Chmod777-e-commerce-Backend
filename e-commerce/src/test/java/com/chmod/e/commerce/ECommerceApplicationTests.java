bc3ca7c2fd72f4d867271b86eb2b0e3f8b387c7d
VIctor Toriz <20020300@es.uveg.edu.mx>
2022-03-30 16:51:54 -0600

    Genere un nuevo proyecto Spring Boot utilizando Spring Initializer e incluyendo Web dependency

---
+++ e-commerce/src/test/java/com/chmod/e/commerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chmod.e.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
